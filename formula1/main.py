871b823791f021db21c9d876ebf26705170b8284
RomanLukianchyk <19arrow19@gmail.com>
2023-07-01 17:33:35 +0200

    slice

--- formula1/main.py
+++ formula1/main.py
@@ -1,22 +1,40 @@
-
 def build_report():
-    racers = {}
+    start_values = {}
+    end_values = {}
+    best_time_report = {}
+
     with open('C:/Foxminded/data.t6/start.txt', 'r') as start:
         for line in start:
-            racer1 = line[:3]
-            time1 = line[14:]
-            racers[racer1] = time1
+            line = line.strip()
+            if line:
+                racer1 = line
+                racer_id_start = racer1[:3]
+                racer_time_start = racer1[14:]
+                if racer_time_start not in start_values or racer_time_start < start_values[racer_id_start]:
+                    start_values[racer_id_start] = racer_time_start
+
     with open('C:/Foxminded/data.t6/end.txt', 'r') as end:
         for line in end:
-            racer2 = line[:3]
-            time2 = line[14:]
-            racers[racer2] = time2
+            line = line.strip()
+            if line:
+                racer2 = line
+                racer_id_end = racer2[:3]
+                racer_time_end = racer2[14:]
+                if racer_time_end not in end_values or racer_time_end < end_values[racer_id_end]:
+                    end_values[racer_id_end] = racer_time_end
 
+    for racer_id_report, time_report in start_values.items():
+        if racer_id_report in end_values:
+            if time_report < end_values[racer_id_report]:
+                best_time_report[racer_id_report] = time_report
 
+    best_time_report = sorted(best_time_report.items(), key=lambda x: x[1])
 
-# abbreviations = open('C:/Foxminded/data.t6/abbreviations.txt', 'r')
+    return best_time_report
 
 
 if __name__ == "__main__":
-    build_report()
-
+    best_racers_list = build_report()
+    print("Best time for each racer:")
+    for index, (racer_id, time) in enumerate(best_racers_list, start=1):
+        print(f"{index}. {racer_id} {time}")
