422e9e03072bb0ae8bddb0d7ce4b36a9d777bb5b
RomanLukianchyk <19arrow19@gmail.com>
2023-08-18 23:23:05 +0200

    all changes

--- main_functions/building_report.py
+++ main_functions/building_report.py
@@ -22,28 +22,28 @@ def build_report(directory, driver_name=None, sort_order="asc"):
     start_values = read_start_file(directory)
     end_values = read_end_file(directory)
 
-    if driver_name and driver_name in [name for name, _ in abbreviations.values()]:
-        abbreviation, full_name = find_driver(abbreviations, driver_name)
-        if abbreviation:
-            abbreviations = {abbreviation: (full_name, abbreviations[abbreviation][1])}
-
     best_time_report = {}
 
     for racer_id_report, time_report in start_values.items():
-        if racer_id_report in end_values and time_report < end_values[racer_id_report]:
-            formatted_time_report = time_report.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-            full_name, team = abbreviations.get(racer_id_report, ("", ""))
-            best_time_report[racer_id_report] = (formatted_time_report, full_name, team)
-
-    if driver_name is None:
-        if sort_order == "asc":
-            best_time_report = dict(sorted(best_time_report.items(), key=lambda x: x[1]))
-        elif sort_order == "desc":
-            best_time_report = dict(sorted(best_time_report.items(), key=lambda x: x[1], reverse=True))
+        if racer_id_report in end_values:
+            if time_report >= end_values[racer_id_report]:
+                # formatted_time_report = time_report.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+                full_name, team = abbreviations.get(racer_id_report, ("", ""))
+                best_time_report[racer_id_report] = ("ERROR!", full_name, team)
+            else:
+                formatted_time_report = time_report.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+                full_name, team = abbreviations.get(racer_id_report, ("", ""))
+                best_time_report[racer_id_report] = (formatted_time_report, full_name, team)
 
     if driver_name:
         driver_stats = get_driver_statistics(best_time_report, driver_name)
         if driver_stats:
             return best_time_report, driver_stats
+        else:
+            return best_time_report, None
     else:
+        if sort_order == "asc":
+            best_time_report = dict(sorted(best_time_report.items(), key=lambda x: x[1]))
+        elif sort_order == "desc":
+            best_time_report = dict(sorted(best_time_report.items(), key=lambda x: x[1], reverse=True))
         return best_time_report, None
--- main_functions/printing_report.py
+++ main_functions/printing_report.py
@@ -7,13 +7,26 @@ def print_report(best_racers_list, driver_stats=None):
         sys.exit()
 
     if driver_stats:
-        print(f"\nСтатистика для гонщика '{driver_stats['name']}':")
-        print(f"Гонщик: {driver_stats['name']}")
-        print(f"Команда: {driver_stats['team']}")
-        print(f"Лучшее время: {driver_stats['time']}")
+        full_name = driver_stats.get('name', "")
+        team = driver_stats.get('team', "")
+        time = driver_stats.get('time', "")
+
+        print(f"\nСтатистика для гонщика '{full_name}':")
+        print(f"Гонщик: {full_name}")
+        print(f"Команда: {team}")
+        if time == "ERROR!":
+            print(f"Лучшее время: {time} (Ошибка: время финиша меньше времени старта)")
+        else:
+            print(f"Лучшее время: {time}")
     else:
         print("Лучшее время для каждого гонщика:")
         for index, (racer_id, (time, full_name, team)) in enumerate(best_racers_list.items(), start=1):
-            print(f"{index}. {full_name.ljust(20)}| {team.ljust(30)}| {time}")
+            full_name = full_name if full_name else ""
+            team = team if team else ""
+
+            if time == "ERROR!":
+                print(f"{index}. {full_name.ljust(20)}| {team.ljust(30)}| {time} (Ошибка: время финиша меньше времени старта)")
+            else:
+                print(f"{index}. {full_name.ljust(20)}| {team.ljust(30)}| {time}")
             if index >= 10:
                 break
