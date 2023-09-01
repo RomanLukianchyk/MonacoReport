db0ddb622a3ee06fbc4d6c26311de19694d1ef89
RomanLukianchyk <19arrow19@gmail.com>
2023-09-01 12:08:05 +0200

    building_report fixed

--- main_functions/building_report.py
+++ main_functions/building_report.py
@@ -13,29 +13,20 @@ def build_report(directory, driver_name=None, sort_order="asc"):
     start_values = read_start_file(directory)
     end_values = read_end_file(directory)
 
+    if driver_name:
+
+        driver_key, full_name = find_driver(abbreviations, driver_name)
+        abbreviations = {driver_key: (full_name, abbreviations[driver_key][1])}
+
     best_time_report = {}
 
-    if driver_name:
-        try:
-            driver_key, full_name = find_driver(abbreviations, driver_name)
-        except ValueError as e:
-            print(e)
-            return
-
+    for driver_key, (full_name, team) in abbreviations.items():
         time_report = start_values.get(driver_key)
         if driver_key in end_values and time_report >= end_values[driver_key]:
-            best_time_report[driver_key] = ("ERROR!", full_name, abbreviations[driver_key][1])
+            best_time_report[driver_key] = ("ERROR!", full_name, team)
         else:
             formatted_time_report = time_report.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-            best_time_report[driver_key] = (formatted_time_report, full_name, abbreviations[driver_key][1])
-    else:
-        for driver_key, (full_name, team) in abbreviations.items():
-            time_report = start_values.get(driver_key)
-            if driver_key in end_values and time_report >= end_values[driver_key]:
-                best_time_report[driver_key] = ("ERROR!", full_name, team)
-            else:
-                formatted_time_report = time_report.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-                best_time_report[driver_key] = (formatted_time_report, full_name, team)
+            best_time_report[driver_key] = (formatted_time_report, full_name, team)
 
     best_time_report = dict(sorted(best_time_report.items(), key=lambda x: x[1], reverse=(sort_order == "desc")))
     return best_time_report, None
